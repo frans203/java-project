f26bbde456dabbb16562705a923478a11a0c0673
Francisco Santana <franciscosantana203@gmail.com>
2022-09-28 20:39:18 -0300

    study of tool JOPTIONPANE

---
+++ studyOfJoption/StudyJoption.java
@@ -0,0 +1,35 @@
+package studyOfJoption;
+
+import javax.swing.*;
+
+public class StudyJoption {
+    public static void main(String[] args){
+//        JOptionPane.showMessageDialog(null, "This is message", "Title", JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "This is message", "Title", JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "This is message", "Title", JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "This is message", "Title", JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "This is message", "Title", JOptionPane.ERROR_MESSAGE);
+//        int result =
+//                JOptionPane.showConfirmDialog(null, "Message Data", "title", JOptionPane.YES_NO_CANCEL_OPTION);
+//        System.out.println(result);
+
+//        String name = JOptionPane.showInputDialog("What is your name: ");
+//        System.out.println(name);
+//
+//        String age = JOptionPane.showInputDialog("What is your age: ");
+//        Double.parseDouble(age);
+//        System.out.println(age);
+
+//        String resposes[] = {"no, you're awesome", "thanks", "test"};
+//
+//        JOptionPane.showOptionDialog(null,
+//                "message", "title",
+//                JOptionPane.YES_NO_CANCEL_OPTION,
+//                JOptionPane.INFORMATION_MESSAGE,
+//                null,
+//                resposes,
+//                0);
+
+
+    }
+}
